c489e6402718b269dc352375103e3cb017ba8774
Sophie Szymaniak <sophiem15@hotmail.com>
2024-11-03 20:10:36 -0500

    outline

--- haxsite-search.js
+++ haxsite-search.js
@@ -3,6 +3,7 @@
  * @license Apache-2.0, see LICENSE for full text.
  */
 import { LitElement, html, css } from "lit";
+import "./hax-card.js";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
 
@@ -20,57 +21,115 @@ export class HaxsiteSearch extends DDDSuper(I18NMixin(LitElement)) {
 
   constructor() {
     super();
-    this.title = "";
-    this.t = this.t || {};
-    this.poop = ""
-    this.t = {
-      ...this.t,
-      title: "Title",
-    };
-    this.registerLocalization({
-      context: this,
-      localesPath:
-        new URL("./locales/haxsite-search.ar.json", import.meta.url).href +
-        "/../",
-      locales: ["ar", "es", "hi", "zh"],
-    });
+    this.value = null;
+    this.title = '';
+    this.loading = false;
+    this.items = [];
   }
 
   // Lit reactive properties
   static get properties() {
     return {
-      ...super.properties,
       title: { type: String },
+      loading: { type: Boolean, reflect: true },
+      items: { type: Array, },
+      value: { type: String },
     };
   }
 
   // Lit scoped styles
   static get styles() {
-    return [super.styles,
-    css`
+    return css`
       :host {
         display: block;
-        color: var(--ddd-theme-primary);
-        background-color: var(--ddd-theme-accent);
-        font-family: var(--ddd-font-navigation);
       }
-      .wrapper {
-        margin: var(--ddd-spacing-2);
-        padding: var(--ddd-spacing-4);
+      :host([loading]) .results {
+        opacity: 0.1;
+        visibility: hidden;
+        height: 1px;
       }
-      h3 span {
-        font-size: var(--haxsite-search-label-font-size, var(--ddd-font-size-s));
+      .results {
+        visibility: visible;
+        height: 100%;
+        opacity: 1;
+        transition-delay: .5s;
+        transition: .5s all ease-in-out;
+        justify-content: center;
+        gap: 90px;
       }
-    `];
+
+      details {
+        margin: 16px;
+        padding: 16px;
+        background-color: blue;
+      }
+      summary {
+        font-size: 24px;
+        padding: 8px;
+        color: white;
+        font-size: 42px;
+        color: var(--ddd-theme-default-coalyGray)''
+      }
+      input {
+        font-size: 20px;
+        line-height: 40px;
+        width: 100%;
+      }
+    `;
   }
 
   // Lit render the HTML
   render() {
     return html`
-<div class="wrapper">
-  <h3><span>${this.t.title}:</span> ${this.title}</h3>
-  <slot></slot>
-</div>`;
+    <h2>${this.title}</h2>
+    <details open>
+      <summary>Search inputs</summary>
+      <div>
+        <input id="input" placeholder="Enter Site Location" @input="${this.inputChanged}" />
+      </div>
+    </details>
+    <div class="results">
+
+
+      ${this.items.map((item, index) => html`
+      <hax-card
+        source="${item.links[0].href}"
+        title="${item.data[0].title}"
+        alt="${item.data[0].title}"
+        secondary_creator= "${item.data[0].secondary_creator}"
+      ></hax-card>
+      `)}
+    </div>
+    `;
+  }
+
+  inputChanged(e) {
+    this.value = this.shadowRoot.querySelector('#input').value;
+  }
+  // life cycle will run when anything defined in `properties` is modified
+  updated(changedProperties) {
+    // see if value changes from user input and is not empty
+    if (changedProperties.has('value') && this.value) {
+      this.updateResults(this.value);
+    }
+    else if (changedProperties.has('value') && !this.value) {
+      this.items = [];
+    }
+    // @debugging purposes only
+    if (changedProperties.has('items') && this.items.length > 0) {
+      console.log(this.items);
+    }
+  }
+
+  updateResults(value) {
+    this.loading = true;
+    fetch(`https://images-api.nasa.gov/search?media_type=image&q=${value}`).then(d => d.ok ? d.json() : {}).then(data => {
+      if (data.collection) {
+        this.items = [];
+        this.items = data.collection.items;
+        this.loading = false;
+      }
+    });
   }
 
   /**
